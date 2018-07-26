090fec7ccbd383d8509f94746c94c6e3a13c8352
Benjamin Trent <ben.w.trent@gmail.com>
2018-07-26 10:53:00 -0500

    Finishing Chapter 1 exercises

--- src/paip_clj/chap_1/ex_1.clj
+++ src/paip_clj/chap_1/ex_1.clj
@@ -2,6 +2,9 @@
   (:require [clojure.string :as str]))
 
 (defn last-name [full-name]
+  "Exercise 1.1: an improved version of `last-name`. 
+  Gives the `last-name` of the given `full-name`.
+  Ex: (last-name '(John Smith Jr. MD)) = Smith"
   (loop [full-name full-name]
     (let [s-name (str (last full-name))]
       (if (str/ends-with? s-name ".")
@@ -11,6 +14,8 @@
           (last full-name))))))
 
 (defn power [number expo]
+  "Exercise 1.2: naive implementation of raising 
+   `number` to the `expo` power."
   (if (= expo 0)
     1
     (loop [total 1
@@ -20,9 +25,20 @@
         (recur (* total number) (- expo 1))))))
 
 (defn atom-count [atoms]
+  "Exercise 1.3: Counts the total number of `atoms` in
+  a passed expression."
   (loop [total 0
          atoms atoms]
     (if (empty? atoms)
       total
       (recur (+ 1 total) (rest atoms)))))
+
+(defn count-anywhere [exp1 exp2]
+  "Exercise 1.4: Count the number of times an expression `exp1`
+  occurs within another expression `exp2`."
+  (count (filter #(= % exp1) (flatten exp2))))
+
+(defn dot-product [nums1 nums2]
+  "Exercise 1.5: Compute the dot-product of `nums1` and `nums2`."
+  (reduce + (map * nums1 nums2)))
 
