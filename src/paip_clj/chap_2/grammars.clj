a38c5ad15d47df15c0b0ee1b8a1e40186fef4221
Benjamin Trent <ben.w.trent@gmail.com>
2018-08-10 15:37:06 -0500

    Updating chap_2 examples

--- src/paip_clj/chap_2/grammars.clj
+++ src/paip_clj/chap_2/grammars.clj
@@ -21,10 +21,7 @@
 (declare noun-phrase)
 
 (defn PP []
-  (concat (Prep) (noun-phrase)))
-
-(defn PP* []
-  (if (rand-nth '(true nil))
+  (concat (Prep) (noun-phrase))) (defn PP* [] (if (rand-nth '(true nil))
     (concat (PP) (PP*))
     nil))
 
@@ -42,23 +39,54 @@
 (defn sentence []
   (concat (noun-phrase) (verb-phrase)))
 
+
 (def simple-grammar
   {
-    :sentence [[noun-phrase verb-phrase]]
-    :noun-phrase [[:article :noun]]
-    :verb-phrase [[:verb noun-phrase]]
+    :sentence [[:noun-phrase :verb-phrase]] 
+    :noun-phrase [[:article :noun ]]
+    :verb-phrase [[:verb :noun-phrase]]
     :article ["the" "a"]
     :noun ["man" "ball" "woman" "table"]
     :verb ["hit" "took" "saw" "liked"]
   }
 )
- 
+
+(def bigger-grammar
+  {
+    :sentence [[:noun-phrase :verb-phrase]] 
+    :noun-phrase [[:article :adj* :noun :prep-phrase*]]
+    :verb-phrase [[:verb :noun-phrase :prep-phrase*]]
+    :prep-phrase* [[] [:prep-phrase :prep-phrase*]]
+    :prep-phrase [[:prep :noun-phrase]]
+    :prep ["to" "in" "by" "with" "on"]
+    :adj* [[] [:adj :adj*]]
+    :adj ["big" "little" "blue" "green" "adiabatic"]
+    :article ["the" "a"]
+    :noun ["man" "ball" "woman" "table"]
+    :verb ["hit" "took" "saw" "liked"]
+  }
+)
+
 (def grammar simple-grammar)
 
-(defn generate [phrase] ;this is not quiet right...something is off
+(defn generate [phrase]
   (cond 
     (vector? phrase) (mapcat generate phrase)
     (keyword? phrase) (generate (rand-nth (phrase grammar)))
     :else (vector (if (fn? phrase) (phrase) phrase))))
 
+(defn generate-tree [phrase]
+  (cond
+        (vector? phrase) (mapcat generate-tree phrase)
+        (keyword? phrase) (cons phrase (generate-tree (rand-nth (phrase grammar))))
+        :else (vector phrase)))
+
+
+(defn generate-all [phrase] ;currently getting a stack overflow?
+  (cond 
+        (nil? phrase) [nil]
+        (vector? phrase) (for [x (generate-all (first phrase)) 
+                               y (generate-all (into [] (rest phrase)))] (vector x y))
+        (keyword? phrase) (mapcat generate-all (phrase grammar))
+        :else (vector (vector phrase))))
 
